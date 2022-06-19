41d84ce1ce4ab39c6745ebebeffa6f2bae6cbf23
lgeu <40058663+Lgeu@users.noreply.github.com>
2022-06-19 18:09:45 +0900

    optimize MakeFeature

--- kore_extension.cpp
+++ kore_extension.cpp
@@ -3,13 +3,8 @@
 // from https://qiita.com/termoshtt/items/81eeb0467d9087958f7f
 #include <boost/python/numpy.hpp>
 
-#include <algorithm>
-#include <boost/python/numpy/dtype.hpp>
-#include <boost/python/numpy/ndarray.hpp>
-#include <boost/python/tuple.hpp>
 #include <fstream>
 #include <stdexcept>
-#include <tuple>
 
 #include "kore_fleets.cpp"
 
@@ -17,8 +12,39 @@ namespace p = boost::python;
 namespace np = boost::python::numpy;
 
 // ファイル名を受け取って、特徴と勝敗を返す
-auto MakeFeature(const string& filename) {
+auto MakeFeature(const string& filename, np::ndarray& out_local_features,
+                 np::ndarray& out_global_features, np::ndarray& out_targets) {
     // 接戦なとこだけ返す
+
+    if (out_local_features.get_nd() != 4 ||
+        out_local_features.shape(0) != 400 ||
+        out_local_features.shape(1) != Feature::kNLocalFeatures ||
+        out_local_features.shape(2) != kSize ||
+        out_local_features.shape(3) != kSize) {
+        throw runtime_error(string("local_features の shape が (400, ") +
+                            to_string(Feature::kNLocalFeatures) + string(", ") +
+                            to_string(kSize) + string(", ") + to_string(kSize) +
+                            string(") じゃないよ"));
+    }
+    if (out_local_features.get_dtype() != np::dtype::get_builtin<float>()) {
+        throw runtime_error("local_features の dtype が float32 じゃないよ");
+    }
+    if (out_global_features.get_nd() != 2 ||
+        out_global_features.shape(0) != 400 ||
+        out_global_features.shape(1) != Feature::kNGlobalFeatures) {
+        throw runtime_error(string("global_features の shape が (400, ") +
+                            to_string(Feature::kNGlobalFeatures) +
+                            string(") じゃないよ"));
+    }
+    if (out_global_features.get_dtype() != np::dtype::get_builtin<float>()) {
+        throw runtime_error("global_features の dtype が float32 じゃないよ");
+    }
+    if (out_targets.get_nd() != 1 || out_targets.shape(0) != 400) {
+        throw runtime_error(string("targets の shape が (400,) じゃないよ"));
+    }
+    if (out_targets.get_dtype() != np::dtype::get_builtin<bool>()) {
+        throw runtime_error("targets の dtype が bool じゃないよ");
+    }
 
     auto is = ifstream(filename);
     if (!is) {
@@ -40,11 +66,13 @@ auto MakeFeature(const string& filename) {
     state.Read(is);
 
     auto n_data = 0;
-    static auto local_features =
-        nn::TensorBuffer<float, 800, Feature::kNLocalFeatures, kSize, kSize>();
-    static auto global_features =
-        nn::TensorBuffer<float, 800, Feature::kNGlobalFeatures>();
-    static auto targets = array<bool, 800>();
+    auto local_features =
+        nn::TensorSlice<float, 800, Feature::kNLocalFeatures, kSize, kSize>(
+            (float*)out_local_features.get_data());
+    auto global_features =
+        nn::TensorSlice<float, 800, Feature::kNGlobalFeatures>(
+            (float*)out_global_features.get_data());
+    auto targets = (bool*)out_targets.get_data();
 
     while (true) {
         is >> line; // "---"
@@ -62,12 +90,11 @@ auto MakeFeature(const string& filename) {
         if (state.step_ < 100 ||
             max(approx_scores[0], approx_scores[1]) <
                 3.0 * min(approx_scores[0], approx_scores[1])) {
-            for (PlayerId player_id = 0; player_id < 2; player_id++) {
-                const auto feature = Feature(state, player_id);
-                local_features[n_data] = feature.local_features;
-                global_features[n_data] = feature.global_features;
-                n_data++;
-            }
+            const auto player_id = (PlayerId)0;
+            const auto feature = Feature(state, player_id);
+            local_features[n_data] = feature.local_features;
+            global_features[n_data] = feature.global_features;
+            n_data++;
         }
     }
 
@@ -75,31 +102,11 @@ auto MakeFeature(const string& filename) {
     is >> line; // "-1"
     double reward0, reward1;
     is >> reward0 >> reward1;
-    for (auto i = 0; i < n_data; i += 2) {
-        if (reward0 > reward1) {
-            targets[i] = true;
-            targets[i + 1] = false;
-        } else {
-            targets[i] = false;
-            targets[i + 1] = true;
-        }
+    for (auto i = 0; i < n_data; i++) {
+        targets[i] = reward0 > reward1;
     }
 
-    // TODO: Boost との接続
-    auto np_local_features =
-        np::empty(p::make_tuple(n_data, Feature::kNLocalFeatures, kSize, kSize),
-                  np::dtype::get_builtin<float>());
-    auto np_global_features =
-        np::empty(p::make_tuple(n_data, Feature::kNGlobalFeatures),
-                  np::dtype::get_builtin<float>());
-    auto np_targets =
-        np::empty(p::make_tuple(n_data), np::dtype::get_builtin<bool>());
-    memcpy(np_local_features.get_data(), local_features.Data(),
-           sizeof(float) * Feature::kNLocalFeatures * kSize * kSize * n_data);
-    memcpy(np_global_features.get_data(), global_features.Data(),
-           sizeof(float) * Feature::kNGlobalFeatures * n_data);
-    memcpy(np_targets.get_data(), targets.data(), sizeof(bool) * n_data);
-    return p::make_tuple(np_local_features, np_global_features, np_targets);
+    return n_data;
 }
 
 /*
@@ -127,3 +134,28 @@ BOOST_PYTHON_MODULE(kore_extension) {
     np::initialize();
     p::def("make_feature", MakeFeature);
 }
+
+#ifdef TEST_KORE_EXTENSION
+int main() {
+    Py_Initialize();
+    np::initialize();
+
+    auto np_local_features =
+        np::empty(p::make_tuple(400, Feature::kNLocalFeatures, kSize, kSize),
+                  np::dtype::get_builtin<float>());
+    auto np_global_features =
+        np::empty(p::make_tuple(400, Feature::kNGlobalFeatures),
+                  np::dtype::get_builtin<float>());
+    auto np_targets =
+        np::empty(p::make_tuple(400), np::dtype::get_builtin<bool>());
+
+    for (auto lap = 0; lap < 10; lap++) {
+        cout << "lap " << lap << endl;
+        MakeFeature("36385265.kif", np_local_features, np_global_features,
+                    np_targets);
+    }
+}
+// clang-format off
+// clang++ -std=c++17 -Wall -Wextra -O3 -DTEST_KORE_EXTENSION -fPIC kore_extension.cpp -I/home/user/anaconda3/include/python3.8 /usr/local/lib/libboost_numpy38.a /usr/local/lib/libboost_python38.a -lpython3.8 -L/home/user/anaconda3/lib -g
+// clang-format on
+#endif
